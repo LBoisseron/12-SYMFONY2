f2f66c8a7c0da0057ce96131152a5b6cf28d5d10
LBoisseron <kalaloolb@gmail.com>
2019-11-29 16:04:07 -0400

    w du 29/11/19
    fin du SYMFONY

--- actuNews/src/Controller/UserController.php
+++ actuNews/src/Controller/UserController.php
@@ -1,29 +1,123 @@
 <?php
 namespace App\Controller;
+use App\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
 class UserController extends AbstractController
 {
-    /**
-     * @Route("/connexion.html", name="user_login", methods={"GET|POST"})
-     */
-    public function login()
-    {
-        return $this->render('user/login.html.twig');
-    }
+
+    #-------------------------------FORMULAIRE D'INSCRIPTION-------------------------------#
+
+
     /**
      * @Route("/inscription.html", name="user_register", methods={"GET|POST"})
      */
-    public function register()
+    public function register(Request $request,
+                             UserPasswordEncoderInterface $encoder)
     {
         # 1. création d'un nouvel utilisateur
-        # 2. création du formulaire
-        # 3. verificationde la soumission
-        # 4. encodage du MdP
-        # 5. sauvegarde en BDD
-        # 6. notification flash
-        # 7. redirection sur la page de connexion
+        $user = new User();
+        $user->setRoles(['ROLE_USER']);
 
-        return $this->render('user/register.html.twig');
+        # 2. création du formulaire
+        $form = $this->createFormBuilder($user)
+            ->add('firstname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre prénom'
+                ]
+            ])
+            ->add('lastname', TextType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre nom'
+                ]
+            ])
+            ->add('email', EmailType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre email'
+                ]
+            ])
+            ->add('password', PasswordType::class, [
+                'label' => false,
+                'attr' => [
+                    'placeholder' => 'Saisissez votre mot de passe'
+                ]
+            ])
+            ->add('submit', SubmitType::class, [
+                'label' => "Je m'inscris !",
+                'attr' => [
+                    'class' => 'btn btn-block btn-dark'
+                ]
+            ])
+            ->getForm();
+
+        $form->handleRequest($request);
+
+
+        # 3. verificationde la soumission
+        if ( $form->isSubmitted() && $form->isValid()) {
+
+
+        # 4. encodage du MdP
+        $user->setPassword(
+            $encoder->encodePassword($user, $user->getPassword())
+        );
+
+        # 5. sauvegarde en BDD
+            $em = $this->getDoctrine()->getManager();
+            $em->persist($user);
+            $em->flush();
+
+        # 6. notification flash
+            $this->addFlash('notice',
+                'Félicitations, vous pouvez vous connecter !');
+
+        # 7. redirection sur la page de connexion
+            return $this->redirectToRoute('app_login');
+
+        }
+
+        return $this->render('default/user/register.html.twig', [
+        'form' => $form->createView()
+
+            ]);
+
+    }
+
+    /**
+     * @Route("/connexion.html", name="app_login")
+     */
+    public function login(AuthenticationUtils $authenticationUtils): Response
+    {
+        // if ($this->getUser()) {
+        //     return $this->redirectToRoute('target_path');
+        // }
+
+        // get the login error if there is one
+        $error = $authenticationUtils->getLastAuthenticationError();
+        // last username entered by the user
+        $lastUsername = $authenticationUtils->getLastUsername();
+
+        return $this->render('security/login.html.twig', ['last_username' => $lastUsername, 'error' => $error]);
+    }
+
+    /**
+     * @Route("/deconnexion.html", name="app_logout")
+     */
+    public function logout()
+    {
+        throw new \Exception('This method can be blank - it will be intercepted by the logout key on your firewall');
     }
 }
